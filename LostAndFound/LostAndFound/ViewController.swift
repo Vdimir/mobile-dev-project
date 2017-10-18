a5721645c77c8fa48c136bd6cd003fd9e9a6a851
Vdimir <vcherckasov@yandex.ru>
2017-10-18 12:40:43 +0300

    Initial commit

---
+++ LostAndFound/LostAndFound/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LostAndFound
+//
+//  Created by student on 18.10.17.
+//  Copyright © 2017 SYN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
