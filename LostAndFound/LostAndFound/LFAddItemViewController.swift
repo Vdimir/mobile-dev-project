454860ca33d4a98fa4ef5391ede6cb1f3314d205
Vdimir <vcherckasov@yandex.ru>
2017-11-22 13:37:14 +0300

    improve constraint

--- LostAndFound/LostAndFound/LFAddItemViewController.swift
+++ LostAndFound/LostAndFound/LFAddItemViewController.swift
@@ -8,19 +8,48 @@
 
 import UIKit
 
-class LFAddItemViewController: UIViewController {
-
+class LFAddItemViewController: UIViewController
+    , UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func loadImgBtnClick(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+//
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
 
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any])
+    {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = chosenImage
+       dismiss(animated:true, completion: nil)
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func saveButtonClick(_ sender: Any) {
+        
     }
     
 
